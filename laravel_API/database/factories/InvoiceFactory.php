e8230583c03823a15ea341955cef51f9ef0bfc8c
StarGaze870 <78165860+StarGaze870@users.noreply.github.com>
2022-11-28 16:30:06 +0800

    uploaded project
    completed 44min of the video

---
+++ laravel_API/database/factories/InvoiceFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Customer;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $status = $this->faker->randomElement(['B', 'P', 'V']); // BILLED, PAID, VOID
+
+        return [
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100, 20_000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL
+
+        ];
+    }
+}
